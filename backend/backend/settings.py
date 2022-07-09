90ac5f56301e66d1ef0785cc2a0aab3499921f07
Cristian Sánchez <criiscz08@gmail.com>
2022-07-09 17:11:47 -0500

    Ni-19 setup backend (#4)
    * [NI-19]-Modified settings to allow environment variables and created DRF support
    
    * Deleted unused dependencies
    
    * Duplicated installed apps removed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import sys
+
 import os
 from corsheaders.defaults import default_headers
 
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+
+
     'django.contrib.staticfiles',  # Required for GraphiQL
     'graphene_django',
     'graph'
