4c819ebeed51ce1c4dfd5d54e0eb47fe168074af
Orlando Vargas <nojipiz@protonmail.com>
2022-07-09 21:36:24 -0500

    Added custom colors of project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'white': '#FFFFFF',
+      'black': '#323232',
+      'green': {
+          1: '#B4CE55',
+          2: '#6B903C',
+        },
+      'gray': {
+          1:'#f5f5f5',
+          2: '#DCDCDD',
+          3: '#838383',
+        },
+    },
   },
   plugins: [],
 }
