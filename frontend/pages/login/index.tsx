68aa2c8fa1f869a50da2d56a821ffb69600e747f
Orlando Vargas <nojipiz@protonmail.com>
2022-07-09 22:00:32 -0500

    Setted credentials a pretty nice credentials context

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -1,17 +1,20 @@
-import { createContext, ReactElement } from "react";
+import { createContext, ReactElement, useContext } from "react";
 import ActionButton from "../../components/ActionButton";
 import * as Icon from "react-bootstrap-icons";
 import InputElement from "../../components/InputElement";
 
+const CredentialsContext = createContext<LoginCredentials>({username: "", password: ""});
+
 export default function Login(): ReactElement {
-    const loginContext = createContext<LoginCredentials>({username: "", password: ""});
     return (
-        <div className="flex w-full text-3xl ">
+        <div className="flex w-full text-3xl">
             <div className="flex items-center justify-center w-full h-screen">
               <BrandContainer/>
             </div>
             <div className="flex items-center justify-center w-full h-screen">
-              <LoginBoxContainer/>
+                <CredentialsContext.Provider value={{ username: "", password: "" }}>
+                    <LoginBoxContainer/>
+                </CredentialsContext.Provider>
             </div>
         </div>
     )
@@ -28,17 +31,18 @@ function BrandContainer(): ReactElement {
 }
 
 function LoginBoxContainer():ReactElement{
+    const currentCredentials = useContext(CredentialsContext);
     return (
       <div className="flex flex-col items-center justify-center shadow w-7/12 rounded-lg pt-10 pb-10 min-w-fit">
             <h1 className="w-11/12 font-bold mb-10">Ingresar</h1>
             <form method="post" id="login" className="flex flex-col items-center justify-center w-11/12">
                 <label className="w-full font-bold">Usuario</label>
-                <InputElement type="text" name="user" placeHolder="tuusuario@proveedor.com" onChange={(text: string) => console.log(text)} />
+                <InputElement type="text" name="user" placeHolder="tuusuario@proveedor.com" onChange={(text: string) => currentCredentials.username = text} />
                 <span className="h-5"> </span>
                 <label className="w-full font-bold">Contraseña</label>
-                <InputElement type="password" name="password" placeHolder="*******" onChange={(text: string) => console.log(text)} />
+                <InputElement type="password" name="password" placeHolder="*******" onChange={(text: string) => currentCredentials.password = text} />
                 <span className="h-5"> </span>
-                <ActionButton text="Entrar" dark={true} onClick={() => console.log("sending data to server")}/>
+                <ActionButton text="Entrar" dark={true} onClick={() => console.log(currentCredentials)}/>
             </form>
       </div>
     );
