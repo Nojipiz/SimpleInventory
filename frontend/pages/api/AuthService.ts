0c0359c04c044f0381dacf9cfe969bddcc76158b
Orlando Vargas <nojipiz@protonmail.com>
2022-07-11 00:39:58 -0500

    Added refresh request

--- frontend/pages/api/AuthService.ts
+++ frontend/pages/api/AuthService.ts
@@ -17,6 +17,22 @@ export async function loginRequest(username: string, password: string): Promise<
     return responseJson as Tokens;
 }
 
+export async function refreshRequest(refresh:string): Promise<Tokens> {
+    const body = JSON.stringify({
+        "refresh": refresh,
+    });
+    const response = await fetch(API_URL + "/refresh", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: body
+    })
+    const responseJson = await response.json();
+    return responseJson as Tokens;
+}
+
 export interface Tokens{
     refresh:string,
     access:string
