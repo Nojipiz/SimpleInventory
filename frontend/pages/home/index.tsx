7d7c8237a1597dfd4c20d3abb08268135157a59c
Orlando Vargas <nojipiz@protonmail.com>
2022-07-11 10:25:56 -0500

    Added logout testing button

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,5 +1,12 @@
 import { ReactElement } from "react";
+import useAuth from "../../hooks/useAuth";
 
 export default function Home():ReactElement{
-    return <h1> Todo: Home Page! </h1>
+    const {logout} = useAuth();
+    return (
+       <>
+            <h1>Home Page!</h1>
+            <button onClick={() => logout()}>logout</button>
+        </>
+    )
 }
