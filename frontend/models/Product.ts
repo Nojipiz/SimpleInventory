c7004e4a8b816e47fda6d0285aaf3c977fe6443d
Orlando Vargas <nojipiz@protonmail.com>
2022-07-14 23:24:56 -0500

    Add basic model

---
+++ frontend/models/Product.ts
@@ -0,0 +1,8 @@
+export interface Product{
+    product_id: string,
+    product_name: string,
+    product_description :string
+    product_units: string,
+    product_price: string,
+    product_status: true
+}
