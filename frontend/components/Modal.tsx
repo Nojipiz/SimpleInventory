7b6ad41e54a3465afde669d864791db9fdc41e20
Orlando Vargas <nojipiz@protonmail.com>
2022-07-25 10:23:06 -0500

    Add prevent to scroll

--- frontend/components/Modal.tsx
+++ frontend/components/Modal.tsx
@@ -1,15 +1,15 @@
 import { ReactElement } from "react";
 
-export default function Modal(props:Props):ReactElement{
-   return (
-       <div className="flex items-center justify-center w-screen h-screen bg-black absolute top-0 bg-opacity-50 z-10">
-           <div className="w-1/2 bg-white rounded-xl p-5">
-               {props.children}
-           </div>
+export default function Modal(props: Props): ReactElement {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-black absolute bottom-0 bg-opacity-50 z-10">
+      <div className="w-1/2 bg-white rounded-xl p-5">
+        {props.children}
       </div>
-   )
+    </div>
+  )
 }
 
-interface Props{
-   children: ReactElement | ReactElement[]
+interface Props {
+  children: ReactElement | ReactElement[]
 }
