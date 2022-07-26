6efd784c5e3a5d48295daffdc8a6a481bd6cefa9
Orlando Vargas <nojipiz@protonmail.com>
2022-07-26 09:08:20 -0500

    Refactor clients name to customers name

--- backend/inventory/controllers/CustomerController.py
+++ backend/inventory/controllers/CustomerController.py
@@ -1,13 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from ..models import Clients
+from ..models import Customers
 from ..serializers.CustomersSerializer import CustomerSerializer
-
 
 class CustomerController(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        return Clients.Customers.objects.all()
+        return Customers.objects.all()
