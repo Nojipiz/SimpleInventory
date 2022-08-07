ba21ee4fb837601362d67a32633dee139de7a2e4
Orlando Vargas <nojipiz@protonmail.com>
2022-08-07 00:55:58 -0500

    Add sale dependencies to models

---
+++ frontend/models/SaleDescription.ts
@@ -0,0 +1,9 @@
+export default interface SaleDescription {
+  sale_id?: number;
+  units?: number;
+  quantity?: number;
+  discount?: number;
+  total?: number;
+  product_id?: number;
+  tax_id?: number;
+}
--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,5 +1,5 @@
+import Link from "next/link";
 import { ReactElement } from "react";
-import ActionButton from "../../components/ActionButton";
 import NavBar from "../../components/NavBar";
 
 export default function Home(): ReactElement {
@@ -15,10 +15,11 @@ function HomeContent(): ReactElement {
   return (
     <div className="m-10">
       <h1 className="text-3xl"> Bienvenido Usuario </h1>
-      <ActionButton
-        text="Crear Factura"
-        dark={false} preventDefault={false}
-        onClick={() => console.log("Creaer")} />
+      <Link href={"/create-bill"}>
+        <button className="bg-green-1 text-white p-2 rounded-xl text-2xl w-full">
+          Crear Factura
+        </button>
+      </Link>
       <div className="flex flex-row justify-between ml-28 mr-28 mt-10">
         <LastSales />
         <LastAddedProducts />
